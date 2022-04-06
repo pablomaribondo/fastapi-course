b58d9849d9c5cebc632cd300345f9d00b9774cbf
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-05 21:39:35 -0300

    feat: create get-student endpoint

--- myapi.py
+++ myapi.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name": "john",
+        "age": 17,
+        "class": "year 12"
+    }
+}
 
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
+    return students[student_id]
