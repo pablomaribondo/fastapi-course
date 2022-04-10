089d1a65cd581e44ac9cd56e60857866299b0b11
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-10 16:43:58 -0300

    feat: create get-by-name endpoint

--- myapi.py
+++ myapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -19,3 +20,11 @@ def index():
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
+
+
+@app.get("/get-by-name")
+def get_student(*,name: Optional[str] = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not Found"}
