4eaf0e57b3d86d6d494b3719c29c5b29ee5f8430
Pablo Maribondo <pablomgvieira@gmail.com>
2022-04-10 17:14:35 -0300

    feat: create update-student endpoint

--- myapi.py
+++ myapi.py
@@ -15,8 +15,14 @@ students = {
 
 class Student(BaseModel):
     name: str
-    age: str
+    age: int
     year: str
+
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
 
 
 @app.get("/")
@@ -37,10 +43,25 @@ def get_student(*, student_id: int, name: Optional[str] = None):
     return {"Data": "Not Found"}
 
 
-@app.post("create-student/{student_id}")
+@app.post("/create-student/{student_id}")
 def create_student(student_id: int, student: Student):
     if student_id in students:
         return {"Error": "Student exists"}
 
     students[student_id] = student
+    return students[student_id]
+
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exists"}
+
+    if student.name != None:
+        students[student_id]["name"] = student.name
+    if student.age != None:
+        students[student_id]["age"] = student.age
+    if student.year != None:
+        students[student_id]["year"] = student.year
+
     return students[student_id]
